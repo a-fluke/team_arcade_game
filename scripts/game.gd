c274692eda1e11560eb20858adbeee2a0faf9cdc
Luke Forsman <luke.forsman@gmail.com>
2026-07-23 14:20:23 -0700

    simple menu navigation

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,19 +1,14 @@
 extends Node2D
 
 const MAIN_MENU = preload("res://scenes/menu.tscn")
+const SELECTION_MENU = preload("res://scenes/selection_menu.tscn")
 
 func _ready() -> void:
 	load_main_menu()
 	
 
-func load_main_menu():
-	var menu = MAIN_MENU.instantiate()
-	menu.start_pressed.connect(_on_start_pressed)
-	$Menu_Container.add_child(menu)
-
-
-func load_level(path: String):
-	# Remove menu
+func unload():
+		# Remove menu
 	for child in $Menu_Container.get_children():
 		child.queue_free()
 
@@ -21,10 +16,30 @@ func load_level(path: String):
 	for child in $Level_Container.get_children():
 		child.queue_free()
 
+func load_main_menu():
+	unload()
+	var menu = MAIN_MENU.instantiate()
+	menu.start_pressed.connect(_on_start_pressed)
+	$Menu_Container.add_child(menu)
+
+
+func load_level(path: String):
+	unload()
 	# Add new level
 	var level = load(path).instantiate()
 	$Level_Container.add_child(level)
 
 
+func load_selection_menu():
+	unload()
+	var menu = SELECTION_MENU.instantiate()
+	menu.start_game_pressed.connect(_on_start_game_pressed)
+	$Menu_Container.add_child(menu)
+
+
 func _on_start_pressed():
-	load_level("res://scenes/level_1.tscn")
+	load_selection_menu()
+
+
+func _on_start_game_pressed():
+	load_level($Menu_Container/selection_menu.level_path)
